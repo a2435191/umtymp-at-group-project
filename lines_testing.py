eb3ab6910fcc34e6f2ce80bb432a93bfbc029182
michael <huangmichael150@gmail.com>
2022-02-13 23:57:33 -0600

    intro video w/ permutations and array notation

--- lines_testing.py
+++ lines_testing.py
@@ -4,8 +4,6 @@ import math
 
 class Lines(Scene):
     def construct(self):
-        # After creating the RubiksCube, it may be necessary to scale it to
-        # comfortably see the cube in the camera's frame
         path = Line(start=[0, 0, 0], end=[0, 1, 0], stroke_color='#FFFFFF')
         path.add_line_to(np.array([1, 1, 0]))
         path.add_line_to(np.array([2, 2, 0]))

18f0d8fb327f8d3ba21cff8c1f707b70f24b0ecc
Will Bradley <7458201@isd535.org>
2022-02-14 22:26:48 -0600

    add intro card animation

---
+++ src/manim/permutation_definition_and_notation/section_intro_cards.py
@@ -0,0 +1,23 @@
+from manim import *
+import sys
+
+def draw_intro(self: Scene, section_number: int, subtitle: str) -> None:
+        text = MarkupText(
+            f"Section {section_number}:\n<span size=\"xx-small\">{subtitle}</span>", 
+            font_size=100
+        )
+        self.play(Write(text, run_time=5))
+        self.wait(0.3)
+        self.play(text.animate.shift(DOWN * 10))
+        self.wait()
+
+
+class Intro1(Scene):
+    def construct(self):
+        subtitle = "What is a permutation? (+notation)"
+        draw_intro(self, 1, subtitle)
+
+
+        
+    
+    

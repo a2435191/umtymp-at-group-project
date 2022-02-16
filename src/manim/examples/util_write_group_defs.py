a46cc0fd0659dc5e583caca47130542193f13353
Will Bradley <7458201@isd535.org>
2022-02-15 19:27:57 -0600

    robustified write_group_defs

--- src/manim/examples/util_write_group_defs.py
+++ src/manim/examples/util_write_group_defs.py
@@ -2,7 +2,7 @@ from manim import *
 from typing import List
 
 def write_group_defs(self: Scene, title_str: str, group_name_str: str,
-    elements: List[str], operator_str: str, 
+    elements: List[str], operator_str: str, ellipsis: bool = False,
     title_font_size: int = 72, math_font_size: int = 64, *args, **kwargs) -> None:
     elements_with_commas: List[str] = []
 
@@ -10,6 +10,8 @@ def write_group_defs(self: Scene, title_str: str, group_name_str: str,
         elements_with_commas.append(",")
         elements_with_commas.append(elem)
     elements_with_commas = elements_with_commas[1:]
+    if ellipsis:
+        elements_with_commas.insert(-1, r"\cdots")
     
 
     title = Tex(r"\underline{" + title_str + r"}", font_size=title_font_size).shift(UP * 2)
@@ -21,12 +23,16 @@ def write_group_defs(self: Scene, title_str: str, group_name_str: str,
         r"\right\}, ",
         operator_str,
         r"\right)",
-
         font_size=math_font_size,
         *args, **kwargs
     )
 
-    for elem in math[2:-3:2]:
+    if ellipsis:
+        red_color_range = math[2:-5:2]
+        red_color_range.add(math[-4])
+    else:
+        red_color_range = math[2:-3:2]
+    for elem in red_color_range:
         elem.set_color(RED)
     math[-2].set_color(BLUE)
 

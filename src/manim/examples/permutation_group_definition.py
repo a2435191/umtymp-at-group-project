2908585b79be3afdf04158e674b280f5186c9714
Will Bradley <7458201@isd535.org>
2022-02-15 19:07:35 -0600

    move permutation group definition bc rel import bs

---
+++ src/manim/examples/permutation_group_definition.py
@@ -0,0 +1,25 @@
+from manim import *
+from util_write_group_defs import write_group_defs
+
+class PermutationGroupDefinition(Scene):
+    def construct(self):
+        matrix_one = r"""\begin{bmatrix}
+    1 & 2 & 3 \\
+    1 & 2 & 3
+\end{bmatrix}"""
+        matrix_two = r"""\begin{bmatrix}
+    1 & 2 & 3 \\
+    1 & 3 & 2
+\end{bmatrix}"""
+        matrix_three = r"""\begin{bmatrix}
+    1 & 2 & 3 \\
+    2 & 1 & 3
+\end{bmatrix}"""
+
+
+        template = TexTemplate()
+        template.add_to_preamble(r"\usepackage{amsmath}")
+        
+        write_group_defs(self, "Permutation Group", "G", 
+            [matrix_one, matrix_two, matrix_three], r"\circ", 72, 50, tex_template=template)
+
